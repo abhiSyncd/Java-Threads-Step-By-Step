04fb3006ec528ccca48c2ab1a5be142f991779d5
abhiSyncd <abhi.syncd@gmail.com>
2020-08-12 17:53:54 +0530

    multithreading

--- 2-a-SequentialExecutionOfThreads-UsingJoin/src/main/MyMain.java
+++ 2-a-SequentialExecutionOfThreads-UsingJoin/src/main/MyMain.java
@@ -4,11 +4,22 @@ public class MyMain {
 
 	public static void main(String[] args) throws InterruptedException {
 
-		Runnable r = new MyRunnable();
+		Runnable runnable = () -> {
+			String[] array = { "My", "Name", "is", "Abhishek" };
 
-		Thread thread1 = new Thread(r, "Thread1");
-		Thread thread2 = new Thread(r, "Thread2");
-		Thread thread3 = new Thread(r, "Thread3");
+			for (String str : array) {
+				try {
+					Thread.sleep(2000L);
+					System.out.println(Thread.currentThread().getName() + "-->" + str);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+		};
+
+		Thread thread1 = new Thread(runnable, "Thread1");
+		Thread thread2 = new Thread(runnable, "Thread2");
+		Thread thread3 = new Thread(runnable, "Thread3");
 
 		thread1.start();
 		thread1.join(); // Start thread2, once thread1 is finished
@@ -21,4 +32,3 @@ public class MyMain {
 
 	}
 }
-
