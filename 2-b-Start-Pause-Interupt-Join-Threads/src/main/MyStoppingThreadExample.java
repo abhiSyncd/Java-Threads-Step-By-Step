adf3f99be613fe2f5fd45dbe6910dbe73a98d298
abhiSyncd <abhi.syncd@gmail.com>
2020-08-12 19:05:49 +0530

    Multithreading

--- 2-b-Start-Pause-Interupt-Join-Threads/src/main/MyStoppingThreadExample.java
+++ 2-b-Start-Pause-Interupt-Join-Threads/src/main/MyStoppingThreadExample.java
@@ -3,10 +3,5 @@ package main;
 public class MyStoppingThreadExample {
 
 	public static void main(String[] args) {
-		
-		//https://codepumpkin.com/interrupt-interrupted-isinterrupted-java-multithreading/
-		// Source : https://www.youtube.com/watch?v=_RSAS-gIjGo
-		
 	}
-
 }
