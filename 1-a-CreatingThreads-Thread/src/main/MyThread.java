f01d9fb7ccd48031971f51d2036700f9aebb7ed1
Abhishek <69460867+abhiSyncd@users.noreply.github.com>
2020-08-12 17:31:50 +0530

    Update MyThread.java

--- 1-a-CreatingThreads-Thread/src/main/MyThread.java
+++ 1-a-CreatingThreads-Thread/src/main/MyThread.java
@@ -2,11 +2,6 @@ package main;
 
 public class MyThread extends Thread {
 	public void run() {
-		try {
-		   Thread.sleep(2000L);
-		   System.out.println("Response from Database from seperate thread : " + Thread.currentThread().getName());
-		} catch (InterruptedException e) {
-		   e.printStackTrace();
-		}
+	  System.out.println("Response from Database from seperate thread : " + Thread.currentThread().getName());
 	}
 }
