29548e3edade8ea8f48cc36f4f5accdf95601df0
Abhishek <69460867+abhiSyncd@users.noreply.github.com>
2020-08-13 18:03:17 +0530

    Update MyMain.java

--- 5-b-Deadlock/src/main/MyMain.java
+++ 5-b-Deadlock/src/main/MyMain.java
@@ -2,37 +2,35 @@ package main;
 
 public class MyMain {
 
-	String resource1 = "Java";
-	String resource2 = "UNIX";
+	public static void main(String[] args) {
 
-	Thread trd1 = new Thread("My Thread 1") {
-		public void run() {
+		final Object resource1 = "resource1";
+		final Object resource2 = "resource2";
+
+		Thread t1 = new Thread(() -> {
 			synchronized (resource1) {
-				System.out.println("Thread 1 ->" + resource1);
+				System.out.println("Thread 1: locked resource 1");
 				synchronized (resource2) {
-					System.out.println("Thread 1 ->" + resource1 + resource2);
+					System.out.println("Thread 1: locked resource 2");
 				}
 			}
-		}
-	};
 
-	Thread trd2 = new Thread() {
-		public void run() {
+		});
+
+		Thread t2 = new Thread(() -> {
 			synchronized (resource2) {
-				System.out.println("Thread 2 ->" + resource2);
+				System.out.println("Thread 2: locked resource 2");
 				synchronized (resource1) {
-					System.out.println("Thread 2 ->" + resource2 + resource1);
+					System.out.println("Thread 2: locked resource 1");
 				}
 			}
-		}
-	};
 
-	public static void main(String a[]) {
-		MyMain mdl = new MyMain();
-		mdl.trd1.start();
-		mdl.trd2.start();
+		});
+
+		t1.start();
+		t2.start();
+
 	}
-
 }
 
   // Ex.2)https://gist.github.com/jirkapinkas/ce1285df9a06d7ba094eb78997942563
